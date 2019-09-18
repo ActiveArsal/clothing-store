0ad1ad6123dfc6532342a4ad42b85a4eb58d1ab7
Arsal Mehran <arsalmehran@gmail.com>
2019-09-18 08:07:17 -0700

    moved shop items into their own reducer

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.js';
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect';
+
+import { selectCollections } from '../../redux/shop/shop.selectors';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props);
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
 
-    render() {
-        const {collections} = this.state;
+const ShopPage = ({ collections }) => {
         return(<div className='shop-page'>
             {
                 collections.map(({id, ...otherCollectionProps }) => {
@@ -22,6 +17,10 @@ class ShopPage extends React.Component {
             }
         </div>);
     }
-}
 
-export default ShopPage;
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
+
+export default connect(mapStateToProps)(ShopPage);

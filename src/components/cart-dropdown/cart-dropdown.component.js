2a72a840588e30846154695f938e98586e2e56eb
Arsal Mehran <arsalmehran@gmail.com>
2019-10-16 09:16:40 -0700

    added responsiveness for header and checkout page on smaller screens

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -13,23 +13,23 @@ import { selectCartItems } from '../../redux/cart/cart.selectors';
 const CartDropdown = ({ cartItems, history, dispatch }) => {
     return (
         <div className='cart-dropdown'>
-        <div className='cart-items'>
-            {   
-                cartItems.length ?
-                cartItems.map(cartItem => (
-                <CartItem key={cartItem.id} item={cartItem} />
-                ))
-                :
-                <span className='empty-message'>Cart Empty</span>
-            }
+            <div className='cart-items'>
+                {   
+                    cartItems.length ?
+                    cartItems.map(cartItem => (
+                    <CartItem key={cartItem.id} item={cartItem} />
+                    ))
+                    :
+                    <span className='empty-message'>Cart Empty</span>
+                }
+            </div>
+            <CustomButton onClick={() => {
+                history.push('/checkout')
+                dispatch(toggleCartHidden());
+                }}>
+                GO TO CHECKOUT
+            </CustomButton>
         </div>
-        <CustomButton onClick={() => {
-            history.push('/checkout')
-            dispatch(toggleCartHidden());
-            }}>
-            GO TO CHECKOUT
-        </CustomButton>
-    </div>
     );
 }
 
--- src/pages/checkout/checkout.component.js
+++ src/pages/checkout/checkout.component.js
@@ -15,7 +15,7 @@ const CheckoutPage = ({ cartItems, total }) => (
                 <span>Product</span>
             </div>
             <div className='header-block'>
-                <span>Description</span>
+                <span>Name</span>
             </div>
             <div className='header-block'>
                 <span>Quantity</span>
@@ -34,12 +34,14 @@ const CheckoutPage = ({ cartItems, total }) => (
         }
 
         <div className='total'>Total: ${total}</div>
+        <StripeCheckoutButton price={total} />
+
         <div className='test-warning'>
             *Please use the following test credit card for payments*
             <br />
             4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
         </div>
-        <StripeCheckoutButton price={total} />
+    
     </div>
 )
 

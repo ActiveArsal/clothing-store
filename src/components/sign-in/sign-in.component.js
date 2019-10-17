cc0751b05836fac7ad7941862e478530c39050b5
Arsal Mehran <arsalmehran@gmail.com>
2019-10-17 02:44:36 -0700

    made sign-in page responsive on smaller screens

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -36,8 +36,8 @@ class SignIn extends React.Component {
     render() {
         return(
             <div className='sign-in'>
-                <h2>I already have an account</h2>
-                <span>Sign in with your email and password</span>
+                <h2 className='title'>I already have an account</h2>
+                <span className='subtitle'>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
                     <FormInput 
